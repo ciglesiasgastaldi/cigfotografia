ebb2d063fc346587ce5ed2c8909106e74dcb57f7
ciglesiasgastaldi <ciglesiasgastaldi@gmail.com>
2023-11-09 16:27:43 -0300

    js al form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+ //Menú hamburguesa
+ 
  const icon = document.getElementById("icon");
  const navUl = document.querySelector("nav ul");
 
@@ -12,4 +14,27 @@ navLinks.forEach((link) =>{
         navUl.classList.remove("show");
 
     });
+});
+
+// formulario.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  const formulario = document.querySelector("#contacto form");
+
+  formulario.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const nombre = formulario.querySelector('input[type="text"]').value;
+    const telefono = formulario.querySelector('input[type="number"]').value;
+    const email = formulario.querySelector('input[type="email"]').value;
+    const mensaje = formulario.querySelector('.text-area').value;
+
+    if (nombre.trim() === "" || telefono.trim() === "" || email.trim() === "" || mensaje.trim() === "") {
+      alert("Por favor, completa todos los campos del formulario.");
+      return;
+    }
+
+    alert("Formulario enviado correctamente!");
+    formulario.reset();
+  });
 });
